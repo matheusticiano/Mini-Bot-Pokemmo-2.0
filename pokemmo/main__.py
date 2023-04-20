0f19be70fc409ff95f5653ceb56cc8b5e9ea5e2d
matheusticiano <112220559+matheusticiano@users.noreply.github.com>
2023-04-20 15:30:38 -0300

    Add files via upload

--- pokemmo/main__.py
+++ pokemmo/main__.py
@@ -1,6 +1,6 @@
-from PySide6.QtCore import QObject, Signal, Slot, QTimer
-from PySide6.QtWidgets import QWidget, QLabel, QPushButton, QMainWindow, QGridLayout, QComboBox
-from PySide6.QtGui import QPixmap, QMovie, QFont
+from PySide6.QtCore import QObject, Signal, Slot, QUrl
+from PySide6.QtWidgets import QWidget, QLabel, QPushButton, QMainWindow, QGridLayout, QComboBox, QMessageBox
+from PySide6.QtGui import QPixmap, QMovie, QFont, QIcon, QDesktopServices
 from threading import Thread
 from actions import pesca, checa_pokemon_e_contador_payday, checa_seta, checa_mapa_e_define_spot_payday, checa_mapa_e_define_spot_exp, checa_pokemon_e_contador_exp, sweet_scent
 from time import sleep
@@ -65,12 +65,14 @@ class MainWindow(QMainWindow):  # Define a Mainwindow
         pokemon_emerald = QMovie('pokemmo/files/pokemon_emerald.gif')
         lucario = QPixmap('pokemmo/files/lucario.png')
         ash = QPixmap('pokemmo/files/ash.png')
+        pikachu_icon = QIcon('pokemmo/files/pikachu_icon.png')
+        prof_oak = QPixmap('pokemmo/files/prof_oak.png')
         font = QFont('Arial Black', 12, QFont.Bold)
         font2 = QFont('Arial Black', 10, QFont.Bold)
 
         self.setWindowTitle('Mini-Bot-PokeMMO')
 
-        self.media_farm = QLabel("Média: 0 \nTempo: 0")
+        self.media_farm = QLabel("Média: 0\nTempo: 0")
         self.media_farm.setFont(font2)
         self.label = QLabel("Clique em Iniciar para Farmar :")
         self.label.setFont(font)
@@ -86,11 +88,25 @@ class MainWindow(QMainWindow):  # Define a Mainwindow
         self.pokemon_emerald.setMovie(pokemon_emerald)
         pokemon_emerald.start()
         pokemon_emerald.setPaused(True)
+
         self.btn_start = Button("Iniciar")
         self.btn_pause = Button("Pausar")
         self.btn_stop = Button("Encerrar")
+        self.btn_ok = Button("OK")
+        self.btn_donate = Button("DONATE")
+
         self.combo_box = QComboBox()
         self.combo_box.addItems(["Exp Farm", "Payday Farm"])
+
+        self.message_box = QMessageBox()
+        self.message_box.setIconPixmap(prof_oak)
+        self.message_box.setWindowIcon(pikachu_icon)
+        self.message_box.setFont(font2)
+        self.message_box.setText("Olá! O bot ainda contem alguns bugs e erros e aqui estão algumas dicas de como evita-los.\nEvite ficar pausando enquanto o bot estiver andando, o certo é pausar apenas quando ele estiver pescando ou lutando...\nEsteja com todos os pokemons e itens. Tenha certeza de que colocou seus Tms e Itens nos respectivos atalhos:\n(Bike no Atalho 1#)\n(Mapa no Atalho 2#)\n(Vara de pesca no Atalho 3#)\n(Sweet Scent no Atalho 4#)\n(Teleport no Atalho 5#)")
+        self.message_box.setInformativeText("A primeira vez que voce iniciar o bot ele ira procurar a região onde voce está e leva-lo para o spot , por favor não pause enquanto isso!!\nTenha um bom farm!!")
+        self.message_box.setWindowTitle("Seja bem vindo, Prepare-se para a Aventura!")
+        self.message_box.addButton(self.btn_ok, QMessageBox.ActionRole)
+        self.message_box.exec_()
 
         grid = QGridLayout()
         grid.addWidget(self.label2, 0, 0, 1, 2)
@@ -104,6 +120,7 @@ class MainWindow(QMainWindow):  # Define a Mainwindow
         grid.addWidget(self.btn_start, 4, 0)
         grid.addWidget(self.btn_pause, 4, 1)
         grid.addWidget(self.btn_stop, 4, 2)
+        grid.addWidget(self.btn_donate, 5, 0, 1, 3)
 
         widget = QWidget()
         widget.setLayout(grid)
@@ -112,6 +129,7 @@ class MainWindow(QMainWindow):  # Define a Mainwindow
         self.btn_start.clicked.connect(self.start)
         self.btn_pause.clicked.connect(self.pause)
         self.btn_stop.clicked.connect(self.stop)
+        self.btn_donate.clicked.connect(lambda: QDesktopServices.openUrl("https://picpay.me/matheus.ticiano"))
 
         self.adjustSize()
         self.setFixedSize(430, 500)
--- pokemmo/theme.py
+++ pokemmo/theme.py
@@ -3,6 +3,10 @@ import qdarktheme
 PRIMARY_COLOR = '#1e81b0'
 DARKER_PRIMARY_COLOR = '#16658a'
 DARKEST_PRIMARY_COLOR = '#115270'
+PRIMARY_COLOR_1 = '#f5a5c5'
+DARKER_PRIMARY_COLOR_1 = '#e082ab'
+DARKEST_PRIMARY_COLOR_1 = '#c35f8f'
+
 
 qss = f"""
     QPushButton[cssClass="specialButton"] {{
@@ -34,3 +38,19 @@ def setupTheme():
         },
         additional_qss=qss
     )
+
+
+qss_pink = f"""
+QPushButton[cssClass="specialButtonPink"] {{
+color: #fff;
+background: {PRIMARY_COLOR_1};
+}}
+QPushButton[cssClass="specialButtonPink"]:hover {{
+color: #fff;
+background: {DARKER_PRIMARY_COLOR_1};
+}}
+QPushButton[cssClass="specialButtonPink"]:pressed {{
+color: #fff;
+background: {DARKEST_PRIMARY_COLOR_1};
+}}
+"""
